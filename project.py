f64893db28ab6d082efa9e664f74348d8f9b5872
Aroon Tcholakov <91400861+aroontcholakov@users.noreply.github.com>
2022-04-20 18:21:13 -0400

    Update project.py

--- project.py
+++ project.py
@@ -7,12 +7,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import altair as alt
 
-gdp = pd.read_csv(r'https://raw.githubusercontent.com/aroontcholakov/5544FinalProject/main/gdp%2Bemissions.csv', sep=',')
-gov = pd.read_csv(r'https://raw.githubusercontent.com/aroontcholakov/5544FinalProject/main/government_type%2Bavg_emissions.csv', sep=',')
-capita = pd.read_csv(r'https://raw.githubusercontent.com/aroontcholakov/5544FinalProject/main/emissions_per_capita.csv', sep=',')
+df_emissions = pd.read_csv(r'https://raw.githubusercontent.com/aroontcholakov/5544FinalProject/main/gdp%2Bemissions.csv', sep=',')
+df_gov = pd.read_csv(r'https://raw.githubusercontent.com/aroontcholakov/5544FinalProject/main/government_type%2Bavg_emissions.csv', sep=',')
+df_percapita = pd.read_csv(r'https://raw.githubusercontent.com/aroontcholakov/5544FinalProject/main/emissions_per_capita.csv', sep=',')
 
 
 
 st.title('F')
-st.write(gdp.head())
+st.write(df_emissions.head())
+st.write(df_gdp.columns())
 
