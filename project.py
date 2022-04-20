e41885868cc3b9d773e6748051f9d5e850daecd3
Aroon Tcholakov <91400861+aroontcholakov@users.noreply.github.com>
2022-04-20 17:17:28 -0400

    Update project.py

--- project.py
+++ project.py
@@ -1,10 +1,10 @@
+import streamlit as st
 import pandas as pd
 import random
 import os
 import numpy as np
-import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 import altair as alt
 
-df = pd.read_csv()
+# df = pd.read_csv()
