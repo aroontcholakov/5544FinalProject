af5512b16b02a011c71fb49bf99de873705ed708
Aroon Tcholakov <91400861+aroontcholakov@users.noreply.github.com>
2022-04-20 17:58:20 -0400

    Update project.py

--- project.py
+++ project.py
@@ -7,5 +7,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import altair as alt
 
-# df = pd.read_csv()
+gdp = pd.read_csv(r'https://github.com/aroontcholakov/5544FinalProject/blob/568a6bf6268a16a8c1727ff95564ec5f3d756508/gdp+emissions.csv')
+gov = pd.read_csv(r'https://github.com/aroontcholakov/5544FinalProject/blob/568a6bf6268a16a8c1727ff95564ec5f3d756508/government_type+avg_emissions.csv')
+capita = pd.read_csv(r'https://github.com/aroontcholakov/5544FinalProject/blob/568a6bf6268a16a8c1727ff95564ec5f3d756508/emissions_per_capita.csv')
+
+
+
 st.title('Final Project')
