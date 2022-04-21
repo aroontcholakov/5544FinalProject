9a25b6b69b0cf8745209f626b3185de4e9949af8
Aroon Tcholakov <91400861+aroontcholakov@users.noreply.github.com>
2022-04-20 22:45:02 -0400

    Update project.py

--- project.py
+++ project.py
@@ -32,8 +32,8 @@ chart_2019 = alt.Chart(df_pc_only).mark_circle(size=50, color='blue').encode(
     y=alt.Y('epc_2019:Q'),
     tooltip=['Country', 'epc_2019']
 ).properties(
-    width=500,
-    height=500,
+    width=600,
+    height=400,
     title={
         "text":["Change in CO2 Emissions Per Capita, 1990-2019"]
     }
@@ -56,9 +56,9 @@ line = alt.Chart(df_pc_only).encode(
 figure = line + chart_1990 + chart_2019
 st.altair_chart(figure, use_container_width=True)
 
-col1, col2 = st.columns([3,1])
+col1, col2 = st.columns([2,1])
 # Government Type Plot
-fig, ax = plt.subplots(figsize=(16,16))
+fig, ax = plt.subplots(figsize=(30,15))
 ax = sns.boxplot(x="government type", y="average emissions", data=df_gov)
 ax = sns.swarmplot(x="government type", y="average emissions", data=df_gov, color=".25")
 ax.set_xticklabels(ax.get_xticklabels(),rotation = 10)
